feb9846e3886de9b91f5b50d5f7e65c01ca543da
jean paul <58843994+Hamelet@users.noreply.github.com>
2020-04-02 23:58:59 +0200

    correction v6

---
+++ exo25/main.js
@@ -0,0 +1,10 @@
+function pouet(params1, params2, params3) {
+    console.log(params1 + 5);
+    console.log(params2.age +=4);
+    console.log(params3 = {age: 50});
+}
+let variable1 = 0;
+let variable2 = {age:12};
+let variable3 = {nom: "Norbert"};
+
+pouet(variable1, variable2, variable3);
